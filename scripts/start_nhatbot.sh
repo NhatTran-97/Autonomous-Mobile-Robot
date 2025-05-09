5b8ca786e7cc78f056fe9ecf91a7d4be8d5c6067
NhatTran-97 <nhat.tranduy1997@gmail.com>
2025-05-09 23:49:23 +0700

    Initial commit of scripts

---
+++ scripts/start_nhatbot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DISPLAY=:0
+xhost +local:root
+
+sudo docker run -it \
+    --rm \
+    --name nhatbot_container \
+    --env DISPLAY=$DISPLAY \
+    --env QT_X11_NO_MITSHM=1 \
+    --env ROS_DOMAIN_ID=7 \
+    --network host \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --device /dev/video0 \
+    --device /dev/bus/usb \
+    --privileged \
+    --volume /home/ninhnt/nhatbot_ws:/home/nhatbot_ws \
+    nhatbot:latest \
+    /bin/bash
